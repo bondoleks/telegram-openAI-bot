4740201ff88522c76a254b236ba6e30811567bf0
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2024-09-15 01:36:53 +0300

    Add program

---
+++ src/main/java/ua/bondoleks/telegram_openAI_bot/TelegramOpenAiBotApplication.java
@@ -0,0 +1,13 @@
+package ua.bondoleks.telegram_openAI_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramOpenAiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramOpenAiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/bondoleks/telegram_openAI_bot/TelegramOpenAiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bondoleks.telegram_openAI_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramOpenAiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
